8811f4f2f434c863df609e048dce589cea952871
APCS3rd <APCS3rd@MHS-Math2-27>
2018-08-20 10:43:48 -0700

    First commit to GitHub

---
+++ Hello First Coding Project/src/Hello_W.java
@@ -0,0 +1,15 @@
+/**
+ * Prints message in console
+ * 
+ * @author Sammi Yeung 
+ * @version 8/17/18
+ */
+public class Hello_W
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World! From Sammi");
+        System.out.println("I don't know what's going on");//Display the string
+    }
+}
+
