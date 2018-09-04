2bc060166afb28b60dcc6ddc97c7f8b8cd08156c
APCS3rd <APCS3rd@SammiYeung>
2018-09-04 09:56:07 -0700

    Pikachu image appears when the program is run

---
+++ PikachuIPikU/src/PikachuCodingProject.java
@@ -0,0 +1,13 @@
+/*This will print out Pikachu
+ * @author Sammi Yeung
+ * @version 8/31/18
+ */
+public class PikachuCodingProject {
+	public static void main(String[] args) {
+		System.out.println("Pikachu welcomes you to the world of Pokemon!");
+		System.out.println("\t\t  (\\__/)");		
+		System.out.println("\t\t  (o^.^)");
+		System.out.println("\t\tz(_(\")(\")");	
+	}
+
+}
